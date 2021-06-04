f8e17b67ce5d46db9538770f29afa55ff8b34483
tkyktkmt <tkyktkmt@gmail.com>
2021-06-04 17:33:03 +0900

    jQuery導入

---
+++ app/javascript/hand.js
@@ -0,0 +1,22 @@
+const hand = () => {
+  $(".pai-list").(function () {
+    console.log(this.id)
+  });
+  // const paiList = document.getElementsByClassName("pais");
+  // paiList.addEventListener('click', function() {
+  //   console.log("hello");
+  //   console.log(this)
+  //   // let paiName = this.getElementByName("name")
+  //   // console.log(paiName)
+  // });
+  // for(let i=0; i<=9; i++) {
+  //   const mani = document.getElementById(`man${i}`);
+  //   const soui = document.getElementById(`sou${i}`); 
+  //   const pini = document.getElementById(`pin${i}`);
+  // };
+  // for(let i=1; i<=7; i++) {
+  //   const jii = document.getElementById(`man${i}`);
+  // };
+};
+
+window.addEventListener("load", hand );
