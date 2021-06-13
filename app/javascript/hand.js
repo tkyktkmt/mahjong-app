e03e233b26191fb05fe3e04d17a751c2bbf7faf0
tkyktkmt <tkyktkmt@gmail.com>
2021-06-13 14:20:13 +0900

    副露削除欄の変数名変更

--- app/javascript/hand.js
+++ app/javascript/hand.js
@@ -48,35 +48,37 @@ const hand = () => {
                   return
             };
           };
-          // if ($(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("src")){
-          //   if (paiName == 5.5 || paiName == 15.5 || paiName == 25.5 ) {
-          //     if (!($(`#pon${huroCount}-1 > .huro-pai${huroCount}`).attr("name") == $(`#pon${huroCount}-2 > .huro-pai${huroCount}`).attr("name") == (paiName-0.5))) {
-          //       alert(`Error ：同じ牌を選択してください`);
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).removeAttr("src");
-          //       paiId = $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("id").replaceAll("pai", "")
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("name", paiId)
-          //       return
-          //     };
-          //   } else if(paiName == 5 || paiName == 15 || paiName == 25 ) {
-          //     if (!(paiName==$(`#pon${huroCount}-1 > .huro-pai${huroCount}`).attr("name") && paiName==$(`#pon${huroCount}-2 > .huro-pai${huroCount}`).attr("name"))||
-          //       (paiName==$(`#pon${huroCount}-1 > .huro-pai${huroCount}`).attr("name") && (paiName+0.5)==$(`#pon${huroCount}-2 > .huro-pai${huroCount}`).attr("name"))||
-          //       ((paiName+0.5)==$(`#pon${huroCount}-1 > .huro-pai${huroCount}`).attr("name") && paiName==$(`#pon${huroCount}-2 > .huro-pai${huroCount}`).attr("name"))){
-          //       alert(`Error ：同じ牌を選択してください`);
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).removeAttr("src");
-          //       paiId = $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("id").replaceAll("pai", "")
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("name", paiId)
-          //       return
-          //     };
-          //   } else {
-          //     if (!(paiName==$(`#pon${huroCount}-1 > .huro-pai${huroCount}`).attr("name") && paiName==$(`#pon${huroCount}-2 > .huro-pai${huroCount}`).attr("name"))){
-          //       alert(`Error ：同じ牌を選択してください`);
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).removeAttr("src");
-          //       paiId = $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("id").replaceAll("pai", "")
-          //       $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("name", paiId)
-          //       return
-          //     };
-          //   };
-          // };
+          break;
+        }; 
+      };
+    };
+    
+    //ポン入力モード
+    if (document.getElementById("mode3").checked){
+      for(let x=1; x<=12; x++){
+        if (!$(`#pai${800+x}`).attr("src")) {
+          $(`#pai${800+x}`).attr({src: paiSrc, name: paiName});
+          let huroCount = Math.floor((x+2)/3);
+          let ponList = Array.prototype.slice.call($(`.huro-pai${huroCount}`));
+          let ponEditClassTr = $(`.huro-table${huroCount}-tr`);
+          let ponEditClassTd = `huro-table${huroCount}-td`;
+          let ponEditId = `pon${huroCount}-`
+          sortHand(ponList, ponEditClassTr, ponEditId, ponEditClassTd);
+          //手牌表示欄の空のtdを削除してtdタグの総数を14に保つ
+          $(`.huro-table${huroCount}-td:empty`).remove();
+          //ポン入力時は同じ牌が３牌揃っていなければならない
+          if ($(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("src")){
+            if (!((ponList[0].name == ponList[1].name && ponList[1].name == ponList[2].name) ||
+                 (ponList[0].name == 5 && ponList[1].name == 5 && ponList[2].name == 5.5) ||
+                 (ponList[0].name == 15 && ponList[1].name == 15 && ponList[2].name == 15.5) ||
+                 (ponList[0].name == 15 && ponList[1].name == 25 && ponList[2].name == 25.5) )){
+                  alert(`Error ：同じ牌を選択してください`);
+                  $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).removeAttr("src");
+                  paiId = $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("id").replaceAll("pai", "")
+                  $(`#pon${huroCount}-3 > .huro-pai${huroCount}`).attr("name", paiId)
+                  return
+            };
+          };
           break;
         }; 
       };
@@ -111,12 +113,12 @@ const hand = () => {
       $(this).removeAttr("src")
       paiId = $(this).attr("id").replaceAll("pai", "")
       $(this).attr("name", paiId)
-      let ponList = Array.prototype.slice.call($(`.huro-pai${x}`));
-      let ponEditClassTr = $(`.huro-table${x}-tr`);
-      let ponEditClassTd = `huro-table${x}-td`;
-      let ponEditId = `pon${x}-`
-      sortHand(ponList, ponEditClassTr, ponEditId, ponEditClassTd);
-      //鼻腔欄の空のtdを削除してtdタグの総数を3に保つ
+      let huroList = Array.prototype.slice.call($(`.huro-pai${x}`));
+      let huroEditClassTr = $(`.huro-table${x}-tr`);
+      let huroEditClassTd = `huro-table${x}-td`;
+      let huroEditId = `huro-pai${x}-`
+      sortHand(huroList, huroEditClassTr, huroEditId, huroEditClassTd);
+      //副露表示欄の空のtdを削除してtdタグの総数を3に保つ
       $(`.huro-table${x}-td:empty`).remove();
     })
   }
