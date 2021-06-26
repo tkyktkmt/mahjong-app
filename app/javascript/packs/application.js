a738701e69863fc5246f69178606f4b4ff222a60
tkyktkmt <tkyktkmt@gmail.com>
2021-06-26 10:39:18 +0900

    シャンテン数算出用ファイル作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../status")
 require("../hand")
+require("../shanten")
 require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
